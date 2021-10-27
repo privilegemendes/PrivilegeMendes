cb8725529b1b1724a3c3cb86f9ff0c000bbe83d9
Privilege Mendes <privilegemendes@gmail.com>
2021-10-27 22:32:59 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -4,6 +4,6 @@ public class Cmwrgk {
         System.out.println("wsnxr");
         System.out.println("jnapo");
         System.out.println("qrbst");
-        System.out.println("jyqup");
+        System.out.println("mftfv");
     }
 }
