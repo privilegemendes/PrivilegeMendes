ba62040de21dabed496d25d937627f1db712d5d0
Privilege Mendes <privilegemendes@gmail.com>
2022-05-13 09:31:28 +0000

    add code in files types: .java,.tsx remove code in files types: .java,.tsx

--- content.java
+++ content.java
@@ -12,6 +12,6 @@ public class Cmwrgk {
         System.out.println("djqdk");
         System.out.println("nctoa");
         System.out.println("brope");
-        System.out.println("japfe");
+        System.out.println("xvoho");
     }
 }
