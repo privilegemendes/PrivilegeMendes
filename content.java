645c3bc33ed67ddff2ded4a185a5f12917b2bb8a
Privilege Mendes <privilegemendes@gmail.com>
2023-02-15 21:22:24 +0000

    add code in files types: .yaml,.java,.tsx,.ts remove code in files types: .yaml,.java,.tsx,.ts

--- content.java
+++ content.java
@@ -1,5 +1,5 @@
-public class Crecfk {
+public class Cctntv {
     public static void main() {
-        System.out.println("pvxdt");
+        System.out.println("dnkeq");
     }
 }
--- content.tsx
+++ content.tsx
@@ -1,2 +1,2 @@
 console.log("msegl")
-console.log("rdivr")
+console.log("hrfpw")
